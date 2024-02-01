e79dd1b5a8f569bb2dd6f41a88e25f0ca1e79c0c
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-02-01 11:54:55 +0100

    removed comments from file

--- database.php
+++ database.php
@@ -1,18 +1,5 @@
 <?php
 
-// function get_state() {
-//     return serialize([$_SESSION['hand'], $_SESSION['board'], $_SESSION['player']]);
-// }
-
-// function set_state($state) {
-//     list($a, $b, $c) = unserialize($state);
-//     $_SESSION['hand'] = $a;
-//     $_SESSION['board'] = $b;
-//     $_SESSION['player'] = $c;
-// }
-
-// return new mysqli('db', 'root', '', 'hive');
-
 class Game {
     private $hand;
     private $board;
