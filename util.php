db3a0298926348635cae3eb1c110e347bdc222ac
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-02-02 17:28:50 +0100

    #8 implemented spider

--- util.php
+++ util.php
@@ -134,3 +134,54 @@ function validAnt($board, $from, $to) {
     
     return false;
 }
+
+function validSpider($board, $from, $to) {
+    if ($from === $to) {
+        return false;
+    }
+    // Spider kan 3 keer verplaatsen dus haal hem van het bord
+    unset($board[$from]);
+
+    $visited = [];
+    $tiles = array($from);
+    $tiles[] = null;
+
+    // laatst bezochte tile en hoevaak er al is verplaatst
+    $previousTile = null;
+    $depth = 0;
+    while(!empty($tiles) && $depth < 3) {
+        // krijg de eerste tile
+        $tile = array_shift($tiles);
+        if ($tile == null) {
+            $depth++;
+            $tiles[] = null;
+            if (reset($tiles) == null) {
+                break;
+            } else {
+                continue;
+            }
+        }
+        // is de tile al bezocht
+        if (!in_array($tile, $visited)) {
+            $visited[] = $tile;
+        }
+
+        $b = explode(',', $tile);
+
+        foreach($GLOBALS['OFFSETS'] as $pq) {
+            $p = $b[0] + $pq[0];
+            $q = $b[1] + $pq[1];
+
+            $pos = $p.','.$q;
+            // is tile al bezocht + pos is niet al geweest + pos is beschikbaar + heeft buren
+            if (!in_array($pos, $visited) && $pos != $previousTile && !isset($board[$pos]) && hasNeighBour($board, $pos)) {
+                if ($pos == $to && $depth == 2) {
+                    return true;
+                }
+                $tiles[] = $pos;
+            }
+            $previousTile = $tile;
+        }
+        return false;
+    }
+}
