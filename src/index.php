6695a57dac43c7d4beac86694eb803676532647b
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-02-05 17:14:04 +0100

    removed brackets from echo

--- src/index.php
+++ src/index.php
@@ -199,7 +199,7 @@
         <strong>
             <?php
                 if (isset($_SESSION['error'])) {
-                    echo($_SESSION['error']);
+                    echo $_SESSION['error'];
                     unset($_SESSION['error']);
                 }
             ?>
