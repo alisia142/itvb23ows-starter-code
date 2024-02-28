82a22105add75ba5f52d58d67cf629474c91a979
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-02-28 11:09:10 +0100

    added require autoload

--- tests/BoardTest.php
+++ tests/BoardTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
 use App\Board;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
--- tests/BugsTest.php
+++ tests/BugsTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
 use PHPUnit\Framework\TestCase;
 use App\Pieces\Grasshopper;
 use App\Pieces\Ant;
