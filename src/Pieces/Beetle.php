2d13340bc8d11dbc58111dbececf1d211e04bf8a
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-02-28 11:21:30 +0100

    added beetle

---
+++ src/Pieces/Beetle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Piece;
+
+class Beetle extends Piece
+{
+    public function validMove($from, $to): bool
+    {
+        $board = clone $this->board;
+
+        unset($this->board[$from]);
+
+        if ($from == $to) {
+            return false;
+        } elseif (!$board->slide($from, $to)) {
+            return false;
+        }
+        return true;
+    }
+}
