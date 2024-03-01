fdc29bcebee9b9e5ad72193774db2abbf95271d3
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-03-01 11:12:32 +0100

    added test for beetle

---
+++ tests/BeetleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use App\Pieces\Beetle;
+use App\Board;
+
+class BeetleTest extends TestCase
+{
+    #[Test]
+    public function testInvalidMove()
+    {
+        $board = new Board([
+            '0,0' => [[0, 'B']],
+            '0,1' => [[1, 'Q']],
+            '0,-1' => [[0, 'G']],
+        ]);
+        $beetle = new Beetle($board);
+        $from = '0,0';
+        $to = '0,1';
+
+        $valid = $beetle->validMove($from, $to);
+
+        $this->assertFalse($valid);
+    }
+}
