6faeeadb60d8cda51690d2cf5e22956bc0f4762a
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-03-04 19:19:03 +0100

    refactor grasshopper

--- src/Pieces/Grasshopper.php
+++ src/Pieces/Grasshopper.php
@@ -13,54 +13,23 @@ class Grasshopper extends Piece
         if ($from === $to) {
             return false;
         }
-        $fromExplode = explode(',', $from);
-        $toExplode = explode(',', $to);
-    
-        // Kijk waar de sprinkhaan heen moet
-        if ($fromExplode[1] == $toExplode[1]) {
-            if ($fromExplode[0] > $toExplode[0]) {
-                $offset = [-1,0]; // Sprinkhaan naar links
-            } else {
-                $offset = [1, 0]; // Sprinkhaan naar rechts
-            }
-        } elseif ($fromExplode[0] == $toExplode[0]) {
-            if ($fromExplode[1] > $toExplode[1]) {
-                $offset = [0, -1]; // Sprinkhaan naar boven
-            } else {
-                $offset = [0, 1]; // Sprinkhaan naar beneden
-            }
-        } elseif ($fromExplode[1] == $toExplode[1] - ($fromExplode[0] - $toExplode[0])) {
-            if ($fromExplode[0] > $toExplode[0]) {
-                $offset = [-1, 1]; // Sprinkhaan naar linksbeneden
-            } else {
-                $offset = [1, -1]; // Sprinkhaan naar rechtsboven
-            }
-        }
-    
-        // Positie
-        $p = $fromExplode[0] + $offset[0];
-        $q = $fromExplode[1] + $offset[1];
-    
-        $pos = $p.",".$q;
-        $posExplode = [$p, $q];
-    
-        // Kijk of de positie buren heeft
-        if (!isset($this->board[$pos])) {
+        
+        if (!$board->isPositionEmpty($to)) {
             return false;
         }
-        
-        // Set pos naar eerst mogelijke positie
-        while (isset($this->board[$pos])) {
-            $p = $posExplode[0] + $offset[0];
-            $q = $posExplode[1] + $offset[1];
-    
-            $pos = $p.",".$q;
-            $posExplode = [$p, $q];
+
+        $neighbours = $board->getNeighbours($from);
+
+        if (!in_array($to, $neighbours)) {
+            return false;
         }
-        // Check of positie gelijk is aan de gewenste bestemming
-        if ($pos == $to) {
-            return true;
+
+        $fromC = explode(',', $from);
+        $toC = explode(',', $to);
+        $dist = abs($toC[0] - $fromC[0]) + abs($toC[1] - $fromC[1]);
+        if ($dist != 2) {
+            return false;
         }
-        return false;
+        return true;
     }
 }
