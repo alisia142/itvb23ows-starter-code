c9a2cb27c6c9d287bce806e1ebba6493b9421cfc
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-03-04 17:19:49 +0100

    updated spider

--- src/Pieces/Spider.php
+++ src/Pieces/Spider.php
@@ -9,53 +9,37 @@ class Spider extends Piece
     public function validMove($from, $to): bool
     {
         $board = clone $this->board;
-        
-        if ($from === $to) {
+        unset($this->board[$from]);
+
+        if ($from == $to) {
+            return false;
+        } elseif (!$board->isPositionEmpty($to)) {
+            return false;
+        } elseif (!$board->isNeighbour($from, $to)) {
+            return false;
+        } elseif (!$board->getTiles()[$to]) {
             return false;
         }
-        // Spider kan 3 keer verplaatsen dus haal hem van het bord
-        unset($this->board[$from]);
-    
-        $visited = [];
-        $tiles = array($from);
-        $tiles[] = null;
-    
-        // laatst bezochte tile en hoevaak er al is verplaatst
-        $previousTile = null;
-        $depth = 0;
+        
+    }
 
-        while(!empty($tiles) && $depth < 3) {
-            // krijg de eerste tile
-            $tile = array_shift($tiles);
-            if ($tile == null) {
-                $depth++;
-                $tiles[] = null;
-                // reset array en geef het eerste element terug
-                if (reset($tiles) == null) {
-                    break;
-                } else {
-                    continue;
-                }
+    public function destinationReachableInThreeSlides($board, $curr, $dest, $visited = [], $count = 0) {
+        if ($curr == $dest && $count == 3) {
+            return false;
+        }
+        $neighbours = $board->getNeighbours($curr);
+        $availableNeighbours = array_filter($neighbours, fn($neighbour) => $board->isPositionEmpty($neighbour));
+
+        foreach ($availableNeighbours as $neighbour) {
+            if (in_array($neighbour, $visited)) {
+                continue;
             }
-            // is de tile al bezocht
-            if (!in_array($tile, $visited)) {
-                $visited[] = $tile;
-            }
-    
-            foreach($this->getAllNeighbours($tile) as $neighbour) {
-                // is tile al bezocht + pos is niet al geweest + pos is beschikbaar + heeft buren
-                if (
-                    !in_array($neighbour, $visited) &&
-                    $neighbour != $previousTile &&
-                    $this->isPositionEmpty(neighbour) &&
-                    $this->hasNeighbour($neighbour)
-                ) {
-                    if ($neighbour == $to && $depth == 2) {
-                        return true;
-                    }
-                    $tiles[] = $neighbour;
+            if ($board->slide($curr, $neighbour)) {
+                $visited[] = $curr;
+
+                if ($this->destinationReachableInThreeSlides($board, $neighbour, $dest, $visited, $count+1)) {
+                    return true;
                 }
-                $previousTile = $tile;
             }
         }
         return false;
