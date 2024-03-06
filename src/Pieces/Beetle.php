851b18dfcd8264524dd6a79eb33f5398ca73c985
alisia142 <46481044+alisia142@users.noreply.github.com>
2024-03-06 12:13:08 +0100

    refactor beetle

--- src/Pieces/Beetle.php
+++ src/Pieces/Beetle.php
@@ -9,12 +9,22 @@ class Beetle extends Piece
     public function validMove($from, $to): bool
     {
         $board = clone $this->board;
-
-        $board->removeTile($from);
-
-        if ($from == $to) {
+        // is positie to gelijk aan from
+        if ($from === $to) {
             return false;
-        } elseif (!$board->slide($from, $to)) {
+        }
+        // verwijder de kever van het board
+        $board->removeTile($from);
+        // kijk of de kever maar 1 stap zet
+        [$fromX, $fromY] = explode(',', $from);
+        [$toX, $toY] = explode(',', $to);
+        $distanceX = abs($toX - $fromX);
+        $distanceY = abs($toY - $fromY);
+        if ($distanceX != 1 || $distanceY != 1) {
+            return false;
+        }
+        // is positie to empty
+        if (!$board->isPositionEmpty($to)) {
             return false;
         }
         return true;
